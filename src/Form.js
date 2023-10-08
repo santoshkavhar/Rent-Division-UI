d1ba38ca09d8f34fa3d9e3218566062452a1029b
skavhar1998 <skavhar1998@gmail.com>
2023-10-08 19:14:56 +0530

    Generated table with rows and columns

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Stack, TextField, Button } from '@mui/material';
+import { Stack, TextField, Button, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 import './Form.css'; // Import the CSS file for styling
 
 function Form() {
@@ -8,26 +8,57 @@ function Form() {
     rooms: 1,
   });
 
+  const [tableData, setTableData] = useState(null);
+  const [showTable, setShowTable] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     let intValue = parseInt(value, 10);
-    
+
     if (!isNaN(intValue) && intValue >= 0) {
       setFormData({ ...formData, [name]: intValue });
     }
   };
+
+  const generateTable = () => {
+    const rows = formData.rooms;
+    const columns = formData.renters;
+
+    const table = [];
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      for (let j = 0; j < columns; j++) {
+        row.push(
+          <TableCell key={j}>
+            <TextField
+              type="number"
+              inputProps={{ min: 0, step: 1 }}
+              style={{ width: '100px' }}
+            />
+          </TableCell>
+        );
+      }
+      table.push(<TableRow key={i}>{row}</TableRow>);
+    }
+
+    setTableData(table);
+    setShowTable(true);
+  };
+
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const url = 'https://jsonplaceholder.typicode.com/posts';
+      const dataToSend = { formData, tableData }; // Include both formData and tableData
       const response = await fetch(url, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(dataToSend),
       });
 
       if (response.ok) {
@@ -63,11 +94,32 @@ function Form() {
             onChange={handleChange}
             inputProps={{ min: 0, step: 1 }}
           />
-          <Button variant="contained" color="primary" type="submit">
-            Submit
+          <Button variant="contained" color="primary" onClick={generateTable}>
+            Generate Table
           </Button>
         </Stack>
       </form>
+
+      {showTable && (
+        <>
+          <h3>Generated Table</h3>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {Array.from(Array(formData.renters)).map((_, index) => (
+                  <TableCell key={index}>Renter {index + 1}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {tableData}
+            </TableBody>
+          </Table>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit Table Data
+          </Button>
+        </>
+      )}
     </div>
   );
 }
